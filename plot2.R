d3dd587156bb9c83d54e14acc6597ae58a9b0c89
Ophelia George <oageorge@mail.usf.edu>
2018-03-11 14:33:36 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -13,7 +13,7 @@ x <-paste(powerdata$Date, powerdata$Time)
 powerdata$Time <- strptime(x, "%Y-%m-%d %H:%M:%S")
 
 
-png(filename="plot2.png")
+png(filename="plot2.png",width=480, height=480)
 
 with(powerdata, plot(Time, Global_active_power, type="l",  ylab = "Global Active Power (kilowatts)", xlab = ""))
 
